569cefb36cfe1115ec6bade333e0dd4c4e2b00e5
Mikhail Molchanov <mikelka150@gmail.com>
2024-07-11 14:16:55 +0300

    back fixed

--- back/back.py
+++ back/back.py
@@ -1,5 +1,4 @@
 import requests
-from flask import Flask, jsonify, request
 from fastapi import FastAPI, Query, File, UploadFile, Form
 from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -27,8 +26,6 @@ def read_root():
 @app.get('/video-info')
 def video_info(videoId: str):
     video_id = videoId
-    if not video_id:
-        return jsonify({'error': 'No video ID provided'}), 400
 
     # Call Llama3-8b API or any other provider to get video data
     clickbait_rating, video_summary, comments_summary = fetch_data_from_llama(
