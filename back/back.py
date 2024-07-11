f62ca42f51c665711f35d2679487f7d87a2fc9c5
Mikhail Molchanov <mikelka150@gmail.com>
2024-07-11 21:07:36 +0300

    formatting summary

--- back/back.py
+++ back/back.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Query, File, UploadFile, Form
 from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 import json
+import logging
 from together import AsyncTogether
 import os
 
@@ -45,21 +46,43 @@ async def video_info(videoId: str):
 
 
 async def fetch_data_from_llama(video_id):
+    # TODO: case no words or transcriptions
     content = ContentManager.content(video_id)
+    batches = []
+    last_word = 0
+    tokens_words = 2/3
+    max_tokens = 7600
+    batch_size = int(tokens_words*max_tokens)
+    while last_word < len(content):
+      batches.append(content[last_word: last_word + batch_size])
+      last_word += batch_size
+
+    # NOTE: 5 sec requirements don't give us an opportunity to load more sentences. But we could to load from different pieces of text
+    text = batches[0]
     response = await llama.chat.completions.create(
         model="meta-llama/Llama-3-8b-chat-hf",
         messages=[
             {
-                "content": "You are an ideal video summarizator. You create summaries in one sentence from video text",
+                "content": "You are an ideal video summarizator. You create summaries in one sentence from video text. In result you write only summary without any other sentences. Write result in json format",
                 "role": "assistant"
             },
             {
-                "content": f'Please, create a one-sentence summary of this video text: {content}',
+                "content": f'Please, create a one-sentence summary of this video text: {text}',
                 "role": "user"
             },
         ],
     )
 
     text = response.choices[0].message.content
+    try:
+      text: str = json.loads(text)['summary']
+    except:
+        try:
+          first_bracet = text.index('{')
+          text = text[first_bracet:]
+          text = json.loads(text)['summary']
+        except:
+           logging.error(f'Could not json with processing summary: {text}')
+        logging.error(f'Could not json summary: {text}')
     # return "45/100", "This video explains the basics of AI and its applications.", "This video is very informative and well-explained."
     return ["45/100", text, "Cool Video"]
