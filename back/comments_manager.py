784a6014cef646ce263c2875112fd9d7fd58f5fe
Mikhail Molchanov <mikelka150@gmail.com>
2024-07-12 00:14:40 +0300

    formatting comments

--- back/comments_manager.py
+++ back/comments_manager.py
@@ -1,4 +1,4 @@
-from back.yt_manager import YtManager
+from yt_manager import YtManager
 
 class CommentsManager:
 
@@ -7,6 +7,25 @@ class CommentsManager:
     comments_request = YtManager.youtube.commentThreads().list(part='snippet,replies',
                                                     maxResults=100, videoId=video_id)
     response = comments_request.execute()
-    print(response)
+    # print(response)
 
-    return response
+    if 'items' not in response:
+      return None
+
+    comments = ''
+
+    for comment in response['items']:
+      text = comment['snippet']['topLevelComment']['snippet']['textDisplay']
+      replies = ''
+      if 'replies' in comment:
+        replies = 'Replies:\n'
+        for reply in comment['replies']['comments']:
+          reply_text = reply['snippet']['textDisplay']
+          replies += f'{reply_text}\n'
+      comments += f'Comment: {text}\n{replies}'
+
+
+    # response['items'][0]['snippet']['topLevelComment']['snippet']['textDisplay']
+    # response['items'][3]['replies']['comments'][0]['snippet']['textDisplay']
+
+    return comments
