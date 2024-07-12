e3417ece301ff60dc7c432ff5b3a26ac8a0a3417
Mikhail Molchanov <mikelka150@gmail.com>
2024-07-12 05:28:31 +0300

    youtube useful comments

--- back/managers/yt_manager.py
+++ back/managers/yt_manager.py
@@ -2,6 +2,12 @@ from googleapiclient.discovery import build
 
 import os
 
+'''
+Here we potentially can have
+RateLimit errors
+but its very easy to make a balancer
+like agents balancer
+'''
 class YtManager:
   api_key = os.environ['YT_API_KEY']
   youtube = build('youtube', 'v3', developerKey=api_key)
