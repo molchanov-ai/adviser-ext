0dad939e54413ca1492c073c945857cf82d4b20d
Mikhail Molchanov <mikelka150@gmail.com>
2024-07-12 04:27:27 +0300

    fixed content format

--- back/agents/content_agent.py
+++ back/agents/content_agent.py
@@ -8,19 +8,14 @@ class ContentAgent:
   async def summary(cls, text):
     llama = Balancer.get_llama()
 
-    batches = []
-    last_word = 0
     tokens_words = 2/3
     max_tokens = 7600
-    batch_size = int(tokens_words*max_tokens)
-    while last_word < len(text):
-      batches.append(text[last_word: last_word + batch_size])
-      last_word += batch_size
 
     # NOTE: 5 sec requirements don't give us an opportunity to load more sentences.
     # But we could to load from different pieces of text
     # But the main of the video is going at first max sentences
-    content_text = batches[0]
+    content_text = text[:int(max_tokens*tokens_words)]
+    ret_format = '{"summary": "<your summary>"}'
     response = await llama.chat.completions.create(
         model="meta-llama/Llama-3-8b-chat-hf",
         messages=[
@@ -29,7 +24,7 @@ class ContentAgent:
                 "role": "system"
             },
             {
-                "content": f'Please, create a one-sentence summary of this video text: {content_text}',
+                "content": f'Please, create a one-sentence summary of this video text. Return format: {ret_format}. Video text:\n{content_text}',
                 "role": "user"
             },
         ],
@@ -41,7 +36,8 @@ class ContentAgent:
     except:
         try:
           first_bracet = content_summary.index('{')
-          content_summary = content_summary[first_bracet:]
+          last_bracet = content_summary.rindex('}')
+          content_summary = content_summary[first_bracet:last_bracet+1]
           content_summary = json.loads(content_summary)['summary']
         except:
            logging.error(
